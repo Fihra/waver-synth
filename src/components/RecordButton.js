9dd0459380c4c6281ce238c9e787634cc53f8d32
fihra <fabrofabian@gmail.com>
2022-07-17 17:26:19 -1000

    created record button at a 10 second timer and exports file

--- src/components/RecordButton.js
+++ src/components/RecordButton.js
@@ -1,23 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Tone from 'tone';
 
 const RecordButton = () => {
     const [ recording, setRecording ] = useState(false);
+    const [ countdown, setCountdown] = useState(10);
+    const [ urlFile, setUrlFile ] = useState("");
+
+    const recorder = new Tone.Recorder();
+    const destination = Tone.getDestination();
+    destination.connect(recorder);
+
+    const tick = () => {
+        if(countdown === 0){
+            setCountdown(10);
+        } else {
+            setCountdown(countdown - 1);
+        }
+        
+    }
+
+    useEffect(() => {
+        if(recording) {
+            const timerId = setInterval(() => tick(), 1000);
+            return () => clearInterval(timerId);
+        }
+    }, [recording, countdown]);
 
     const handleRecording =() => {
-        if(recording){
-            setRecording(false);
-        } else {
+        if(recording === false){
             setRecording(true);
+            recorder.start();
+            returnURL();  
         }
+        console.log("dont record again")
     }
-    console.log(recording);
+
+    const returnURL = () => {
+        setTimeout(async () => {
+            console.log("heillo")
+            const recording = await recorder.stop();
+            const url = URL.createObjectURL(recording);
+            const anchor = document.createElement("a");
+            anchor.download = "waver-synth-recording.ogg";
+            anchor.href = url;
+            anchor.click();
+            setRecording(false);
+        }, 11000);
+
+    }
 
     return(
         <div className="record-button">
-        <button className={recording ? "recording-active" : ""} onClick={handleRecording}>
+        <button id="rec-button" className={recording ? "recording-active" : ""} onClick={handleRecording}>
             <span>REC</span>
         </button>
+        <p>Recording time left: {countdown}</p>
         </div>
     )
 }
