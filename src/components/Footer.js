fdc4b4a3e1e349534d4db9d789cb491e2e86e071
fihra <fabrofabian@gmail.com>
2022-07-19 19:20:06 -1000

    edited footer to have desktop and mobile support

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return(
         <footer>
-            <p>Currently on desktop support only.</p>
+            <p>Desktop and Mobile support only.</p>
             <p>Built by Fabian Fabro</p>
         </footer>
     )
